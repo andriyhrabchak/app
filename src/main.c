994788e187920c34d70311a59d55663be99f9b69
andriyhrabchak <andriyhrabchak@gmail.com>
2024-07-28 17:05:47 +0300

    automount littlefs

--- src/main.c
+++ src/main.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 
 #include <zephyr/kernel.h>
-#include <zephyr/fs/fs.h>
 
 #include <app_version.h>
+
 #ifdef CONFIG_GPIO_PCF8574T
 #include <ioexp.h>
 #endif
@@ -17,9 +17,8 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-#define SLEEP_TIME_MS   1000
-
 #ifdef CONFIG_FAT_FILESYSTEM_ELM
+#include <zephyr/fs/fs.h>
 #include <ff.h>
 /* FatFs work area */
 FATFS app_fat_fs;
@@ -31,18 +30,7 @@ static struct fs_mount_t app_fatfs_mnt = {
 };
 #endif
 
-#ifdef CONFIG_FILE_SYSTEM_LITTLEFS
-#include <zephyr/fs/littlefs.h>
-#define STORAGE_PARTITION_LABEL storage_partition
-#define STORAGE_PARTITION_ID FIXED_PARTITION_ID(STORAGE_PARTITION_LABEL)
-FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(cstorage);
-static struct fs_mount_t littlefs_mnt = {
-	.type = FS_LITTLEFS,
-	.fs_data = &cstorage,
-	.storage_dev = (void *)STORAGE_PARTITION_ID,
-	.mnt_point = "/LFS:"
-};
-#endif
+#define SLEEP_TIME_MS   1000
 
 int main(void) {
   int rc = 0;
@@ -53,13 +41,6 @@ int main(void) {
 
   LOG_WRN("'Hello World!' v%s on %s", APP_VERSION_STRING, CONFIG_BOARD);
 
-  #ifdef CONFIG_FILE_SYSTEM_LITTLEFS
-	rc = fs_mount(&littlefs_mnt);
-	if (rc < 0)	{
-		LOG_ERR("Error mounting littlefs [%d]", rc);
-	}
-  #endif
-  
   #ifdef CONFIG_FAT_FILESYSTEM_ELM
 	rc = fs_mount(&app_fatfs_mnt);
   if (rc == -EIO) {
